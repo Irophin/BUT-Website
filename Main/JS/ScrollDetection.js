23165623aa44251a3d1c3a13cfd54491e290094e
Eviive <albert.vaillon21@gmail.com>
2021-11-24 20:32:42 +0100

    Minor changes

--- Main/JS/ScrollDetection.js
+++ Main/JS/ScrollDetection.js
@@ -2,14 +2,14 @@ var lastScrollTop = 0;
 
 $(window).scroll(function(event){
 
-	var sT = $(this).scrollTop();
+	var scrollTop = $(this).scrollTop();
 
-	if (sT > lastScrollTop){
+	if (scrollTop > lastScrollTop) {
     	console.log('scroll down') // pour le debug
 
 	} else {
 		console.log('scroll up') // pour le debug
 	}
 
-	lastScrollTop = sT;
+	lastScrollTop = scrollTop;
 });
