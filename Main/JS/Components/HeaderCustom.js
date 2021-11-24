72b01248b51d7009d1b8a55c561af23e9399ff91
Eviive <albert.vaillon21@gmail.com>
2021-11-24 13:44:36 +0100

    Fix avec flex

--- Main/JS/Components/HeaderCustom.js
+++ Main/JS/Components/HeaderCustom.js
@@ -3,26 +3,26 @@ export default class HeaderCustom extends HTMLElement {
 	constructor () {
 		super()
 		this.innerHTML = `
-			<button class="bouton-menu" onclick="menu()">
-				<div class="hamburger">
-					<span class="barre-1"></span>
-					<span class="barre-2"></span>
-				</div>
-			</button>
-			<div>
-				<h3>
-					<a href="index.html">SAE Web</a>
-				</h3>
-				<div class="wrapper">
-					<input type="checkbox" id="input">
-					<div class="search-bar">
-						<input placeholder="Recherche">
-						<span class="label">Recherche</span>
+			<div class="wrapper-hb">
+				<button class="bouton-menu" onclick="menu()">
+					<div class="hamburger">
+						<span class="barre-1"></span>
+						<span class="barre-2"></span>
 					</div>
-					<label for="input" class="loupe">
-						<img src="../Images/loupe.png" alt="loupe">
-					</label>
+				</button>
+			</div>
+			<h3>
+				<a href="index.html">SAE Web</a>
+			</h3>
+			<div class="wrapper-sb">
+				<input type="checkbox" id="input">
+				<div class="search-bar">
+					<input placeholder="Recherche">
+					<span class="label">Recherche</span>
 				</div>
+				<label for="input" class="loupe">
+					<img src="../Images/loupe.png" alt="loupe">
+				</label>
 			</div>`
 	}
 
