afd5f6e9a9f7d3f3eed2f9750421e5f77249b5ab
Eviive <albert.vaillon21@gmail.com>
2021-12-07 15:40:11 +0100

    Modif du script de la nav avec un while

--- Main/JS/Components/NavCustom.js
+++ Main/JS/Components/NavCustom.js
@@ -1,21 +1,17 @@
 export default class NavCustom extends HTMLElement {
 	constructor () {
 		super()
-		var nb = this.getAttribute('nb')
-		for (let index = 1; index <= nb; index++) {
+		var index = 1
+		while (this.getAttribute(`t${index}`) !== null) {
 			var text = this.getAttribute(`t${index}`)
 			this.innerHTML += `
 				<a href="#section${index}">${text}</a>`
-			if (index === 1) {
-				var first = this.firstElementChild
-				first.classList.add('active')
-			}
+			index++
 		}
+		var first = this.firstElementChild
+		first.classList.add('active')
 	}
 }
 
 // var hauteur = this.clientHeight
 // this.style.setProperty('--hauteur', hauteur + "px");
-
-// faire la même avec la hauteur du header pour le top du hr et le top du parallax
-// et peut-être pour la scroll detection en adaptant les top du hr, du parallax quand .hidden est là
