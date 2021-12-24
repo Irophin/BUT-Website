a21a4523d101e4a00af6928004ced9b469ab4242
Eviive <albert.vaillon21@gmail.com>
2021-12-25 00:18:02 +0100

    Refonte du header

--- Main/JS/Components/HeaderCustom.js
+++ Main/JS/Components/HeaderCustom.js
@@ -12,21 +12,9 @@ export default class HeaderCustom extends HTMLElement {
 			</div>
 			<h3 id="title"></h3>
 			<div class="wrapper-sb">
-				<div class="search-bar">
-					<input placeholder="Recherche" list="suggestions">
-					<span class="label">Recherche</span>
-					<datalist id="suggestions">
-						<option value="Acceuil">
-						<option value="L'IUT">
-						<option value="Le BUT">
-						<option value="Informations">
-						<option value="Prendre RDV">
-						<option value="Contacter d'anciens étudiants">
-					</datalist>
-				</div>
-				<button id="loupe" onclick="searchbar()">
+				<a class="loupe" href="Recherche.html">
 					<img src="../Images/loupe.png" alt="Loupe">
-				</button>
+				</a>
 			</div>`
 		this.h3 = document.getElementById('title')
 		this.h3.innerHTML = document.body.getAttribute('data-page')
