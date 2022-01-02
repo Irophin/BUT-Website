360da32d229bc1725872bdacc4f248a37fe92fb9
Lilian BAUDRY <lilian.baudry21@gmail.com>
2022-01-02 19:50:02 +0100

    Minor changes

--- JS/Accueil.js
+++ JS/Accueil.js
@@ -2,13 +2,13 @@ function Indication(){
 	const indication = document.querySelector('.scroll-indication');	// sélection l'element d'indication de scroll
 
 	if (indication != null) {
-		if (window.scrollY < 10 ){										// Test si l'utilisateur est en haut de la page
+		if (window.scrollY < 25 ){										// Test si l'utilisateur est en haut de la page
 			indication.classList.add('active');							// Si oui active l'indication de scroll
 		}else{
 			indication.classList.remove('active');						// Sinon la désactive
 		}
 	}
-	window.requestAnimationFrame(Haut);
+	window.requestAnimationFrame(Indication);
 }
 
 Indication();
